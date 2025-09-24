f4fd5f531694ae78e3c6000e3289e87e6648a84c
Svitlana <missis.chuvaeva@ukr.net>
2025-09-24 21:29:31 +0300

    form-registration/formik-install

--- app/(auth router)/auth/[authType]/page.tsx
+++ app/(auth router)/auth/[authType]/page.tsx
@@ -1,7 +1,19 @@
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+'use client';
 
-export default page
+import { useParams } from 'next/navigation';
+import RegistrationForm from '../../../../components/RegistrationForm/RegistrationForm';
+import LoginForm from '../../../../components/LoginForm/LoginForm';
+
+export default function AuthPage() {
+  const { authType } = useParams<{ authType: string }>();
+
+  if (authType === 'register') {
+    return <RegistrationForm />;
+  }
+
+  if (authType === 'login') {
+    return <LoginForm />;
+  }
+
+  return <div>Невірний тип аутентифікації</div>;
+}
