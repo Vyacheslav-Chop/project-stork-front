db06a5c3731262934b8711b5c079e65c4c7983f6
Dmitry Zinchenko <zinchenko89d@gmail.com>
2025-09-25 15:40:16 +0300

    Dima Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+import css from './page.module.css';
+import GreetingBlock from '@/components/GreetingBlock/GreetingBlock';
+import StatusBlock from '@/components/StatusBlock/StatusBlock';
+import BabyTodayCard from '@/components/BabyTodayCard/BabyTodayCard';
+import MomTipCard from '@/components/MomTipCard/MomTipCard';
+import TasksReminderCard from '@/components/TasksReminderCard/TasksReminderCard';
+import FeelingCheckCard from '@/components/FeelingCheckCard/FeelingCheckCard';
+import { getBabyState } from '@/lib/api/apiClient';
+import { BabyWeekData } from '@/types/BabyWeekData';
 
-export default page
+
+export default async function Dashboard() {
+  const babyData: BabyWeekData = await getBabyState(14);
+  return (
+    <div className={css.mainWrapper}>
+      <GreetingBlock />
+      <div className={css.innerWrapper}>
+        <div className={css.firstWrapper}>
+          <StatusBlock />
+          <BabyTodayCard data={babyData} />
+          <MomTipCard />
+        </div>
+        <div className={css.lastWrapper}>
+          <TasksReminderCard />
+          <FeelingCheckCard />
+        </div>
+      </div>
+    </div>
+  );
+}
