ce6acf66b83250d178c4801147871b2a9eee805e
Raffaello-creator <wolf201026@gmail.com>
2025-09-25 02:29:01 +0200

    default page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import TasksReminderCard from "@/components/TasksReminderCard/TasksReminderCard";
 
 export default function Home() {
   return (
