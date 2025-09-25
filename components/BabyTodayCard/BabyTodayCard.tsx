cc292fe673635b4a84156d7017e9e5bbc53c5ce2
Nikita2993-Z <zinchenko.nikita93@gmail.com>
2025-09-25 13:50:46 +0300

    BabyTodayCard

--- components/BabyTodayCard/BabyTodayCard.tsx
+++ components/BabyTodayCard/BabyTodayCard.tsx
@@ -1,7 +1,47 @@
-const BabyTodayCard = () => {
-  return (
-    <div>BabyTodayCard</div>
-  )
+import styles from "./BabyTodayCard.module.css";
+import { BabyWeekData } from "@/types/BabyWeekData";
+import Image from "next/image";
+
+interface Props {
+  data: BabyWeekData;
 }
 
-export default BabyTodayCard
+const BabyTodayCard = ({ data }: Props) => {
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Малюк сьогодні</h2>
+
+        <div className={styles.content}>
+          {/* Левая колонка */}
+          <div className={styles.imageBlock}>
+            <Image
+              src={data.image}
+              alt={`Тиждень ${data.weekNumber}`}
+              width={287}
+              height={216}
+            />
+          </div>
+
+          {/* Правая колонка */}
+          <div className={styles.infoBlock}>
+            <p>
+              <strong>Розмір:</strong> Приблизно {data.babySize} см.
+            </p>
+            <p>
+              <strong>Вага:</strong> Близько {data.babyWeight} грамів.
+            </p>
+            <p>
+              <strong>Активність:</strong> {data.babyActivity}
+            </p>
+          </div>
+        </div>
+
+        {/* Текст снизу */}
+        <p className={styles.development}>{data.babyDevelopment}</p>
+      </div>
+    </section>
+  );
+};
+
+export default BabyTodayCard;
