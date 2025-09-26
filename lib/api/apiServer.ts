4c37f77d61dcb56c97f65d0185b4c7f8255d3c20
iren <dudareva.iryna.study@gmail.com>
2025-09-27 00:51:14 +0200

    initial transfer

--- lib/api/apiServer.ts
+++ lib/api/apiServer.ts
@@ -1,1 +1,20 @@
+import { cookies } from "next/headers";
+import { nextServer } from "./api";
+import { WeekData } from "@/types/week";
+import { MomState } from "@/types/momStates";
 
+export async function fetchPrivateWeekDataServer(): Promise<WeekData> {
+  const cookieStore = cookies();
+  const res = await nextServer.get<WeekData>("/weeks/private", {
+    headers: { Cookie: cookieStore.toString() },
+  });
+  return res.data;
+}
+
+export async function fetchMomStateServer(weekNumber: number): Promise<MomState> {
+  const cookieStore = cookies();
+  const res = await nextServer.get<MomState>(`/weeks/mom-state/${weekNumber}`, {
+    headers: { Cookie: cookieStore.toString() },
+  });
+  return res.data;
+}
