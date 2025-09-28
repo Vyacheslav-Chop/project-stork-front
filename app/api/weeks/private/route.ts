2a7a8f8d6af0abb202274bd14cb958320562f68e
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-09-28 12:12:12 +0300

    refactor: add sidebar to layout and simplify header logic

--- app/api/weeks/private/route.ts
+++ app/api/weeks/private/route.ts
@@ -6,14 +6,16 @@ import { cookies } from 'next/headers';
 
 export async function GET() {
   const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+
   try {
-    const { data } = await api.get('/weeks/private', {
+    const { data } = await api.get("/weeks/private", {
       headers: {
-        Cookie: cookieStore.toString(),
+        Authorization: `Bearer ${accessToken}`,
       },
     });
     if (data) return NextResponse.json(data);
-  } catch {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  } catch (err){
+    return NextResponse.json({ error: err.messsage }, { status: err.status });
   }
 }
