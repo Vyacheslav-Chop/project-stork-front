8be21224ceb515c5c89ff8e28e12e4a34c17d2d0
Svitlana <missis.chuvaeva@ukr.net>
2025-09-28 16:20:05 +0300

    onboardingForm, css,layout

---
+++ app/(private router)/profile/edit/layout..tsx
@@ -0,0 +1,8 @@
+export default function OnBoardingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <h2>OnBoarding Edit Layout</h2>
+      {children}
+    </div>
+  );
+}
