ec04ec95b6576941347db21e0f05da34d2208254
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-09-29 19:46:24 +0300

    refactor: move data fetching to Dashboard and simplify StatusBlock & MomTipCard

--- types/week.ts
+++
@@ -1,9 +0,0 @@
-import { BabyWeekData } from "./babyWeekData";
-
-export interface WeekData {
-  currentWeek: number;
-  daysToBirth: number;
-  isPersonalized: boolean;
-  weekData: BabyWeekData;   
-}
-
