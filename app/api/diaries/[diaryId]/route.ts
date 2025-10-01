ef045eec0b2b0772d7c46098f85cb4e31190633b
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-10-01 10:08:07 +0300

    fix bug

--- app/api/diaries/[diaryId]/route.ts
+++ app/api/diaries/[diaryId]/route.ts
@@ -4,9 +4,9 @@ import { api } from "../../api";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { diaryId: string } }
+  { params }: { params: Promise<{ diaryId: string }> }
 ) {
-  const { diaryId } = params;
+  const { diaryId } = await params;
 
   const cookieStore = await cookies();
   const accessToken = cookieStore.get("accessToken")?.value;
@@ -30,9 +30,9 @@ export async function GET(
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { diaryId: string } }
+  { params }: { params: Promise<{ diaryId: string }> }
 ) {
-  const { diaryId } = params;
+  const { diaryId } = await params;
 
   const cookieStore = await cookies();
   const accessToken = cookieStore.get("accessToken")?.value;
@@ -53,9 +53,9 @@ export async function DELETE(
 
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: { diaryId: string } }
+  { params }: { params: Promise<{ diaryId: string }> }
 ) {
-  const { diaryId } =  params;
+  const { diaryId } = await  params;
   const body = await req.json();
 
   const cookieStore = await cookies();
