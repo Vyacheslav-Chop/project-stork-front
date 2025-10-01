f0257bd7f84fb45998e6ad7f5226ba635491f336
Vadym <vadim.drinevskiy@gmail.com>
2025-10-01 14:48:09 +0300

    add edit diary form and fix css bugs

--- components/modals/AddDiaryEntryModal/AddDiaryEntryForm/validation.ts
+++ components/modals/AddDiaryEntryModal/AddDiaryEntryForm/validation.ts
@@ -3,12 +3,13 @@ import * as Yup from "yup";
 export const validationDiarySchema = Yup.object().shape({
   title: Yup.string()
     .min(3, "Мінімум 3 символи")
-    .max(30, "Максимум 30 символів")
+    .max(20, "Максимум 20 символів")
     .required("Заголовок обов’язковий"),
   categories: Yup.array()
     .of(Yup.string())
     .min(1, "Виберіть хоча б одну категорію"),
   description: Yup.string()
-    .min(5, "Мінімум 5 символів")
+    .min(1, "Мінімум 1 символів")
+    .max(1000, "Максимум 1000 символів")
     .required("Запис обов’язковий"),
 });
